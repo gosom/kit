61137a5533ae353a687276efd529f0b41ca3de64
Giorgos Komninos <admin@gkomninos.com>
2023-01-21 09:53:07 +0200

    Adds tests for middleware

--- web/middleware_test.go
+++ web/middleware_test.go
@@ -6,7 +6,9 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
+	"github.com/realclientip/realclientip-go"
 	"github.com/rs/cors"
 	"github.com/stretchr/testify/require"
 
@@ -54,4 +56,63 @@ func TestRequestLoggerMiddleware(t *testing.T) {
 	require.Equal(t, "", v["query"])
 	require.Equal(t, "", v["ip"])
 	require.Equal(t, "", v["user-agent"])
+}
+
+func TestTimeoutMiddleware(t *testing.T) {
+	mw := web.Timeout(50 * time.Millisecond)
+	require.NotNil(t, mw)
+
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for {
+			select {
+			case <-r.Context().Done():
+				ae := lib.NewApiError(http.StatusRequestTimeout, "timeout")
+				web.JSONError(w, r, ae)
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+			}
+		}
+		web.JSON(w, r, http.StatusOK, `{"foo":"bar"}`)
+	})
+	mw(next).ServeHTTP(w, req)
+	require.Equal(t, http.StatusRequestTimeout, w.Code)
+}
+
+func TestRecoverMiddleware(t *testing.T) {
+	var b bytes.Buffer
+	b.Reset()
+	defaultLogger := logging.Get()
+	defer func() {
+		logging.SetDefault(defaultLogger)
+	}()
+	req := httptest.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		panic("test")
+	})
+	web.Recover(next).ServeHTTP(w, req)
+	require.Equal(t, http.StatusInternalServerError, w.Code)
+}
+
+func TestRealIPMiddleware(t *testing.T) {
+	ipStrategy := realclientip.RemoteAddrStrategy{}
+	mw := web.RealIP(ipStrategy)
+	require.NotNil(t, mw)
+	req := httptest.NewRequest("GET", "/", nil)
+	req.RemoteAddr = "127.0.0.1:1234"
+	w := httptest.NewRecorder()
+	next := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		web.JSON(w, r, http.StatusOK, `{"foo":"bar"}`)
+	})
+
+	mw(next).ServeHTTP(w, req)
+	require.Equal(t, http.StatusOK, w.Code)
+
+	ip := lib.IPFromContext(req.Context())
+	require.Equal(t, "127.0.0.1", ip)
 }
